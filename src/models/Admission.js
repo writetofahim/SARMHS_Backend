a6fbd9546fb3d76c755269bc44fe6593ce3cd622
writetofahim <writetofahimahammad@gmail.com>
2023-11-07 11:08:27 +0600

    admission model changed

--- src/models/Admission.js
+++ src/models/Admission.js
@@ -1,15 +1,44 @@
 const mongoose = require("mongoose");
 
 const admissionSchema = new mongoose.Schema({
-  studentName: String,
-  schoolName: String,
-  currentAddress: String,
-  previousClassResult: String,
-  email: String,
-  phone: String,
+  // studentName: String,
+  // schoolName: String,
+  // currentAddress: String,
+  // previousClassResult: String,
+  // email: String,
+  // phone: String,
+  // classWantToTakeAdmission: String,
+
   admissionId: String,
-  classWantToTakeAdmission: String,
   status: String, // Status of the admission (e.g., 'Pending', 'Accepted', 'Declined')
+
+  studentNameBangla: String,
+  studentNameEnglish: String,
+  fatherNameBangla: String,
+  fatherNameEnglish: String,
+  fatherOccupation: String,
+  motherNameBangla: String,
+  motherNameEnglish: String,
+  presentVillage: String,
+  presentPO: String,
+  presentUpazila: String,
+  presentZila: String,
+  permanentVillage: String,
+  permanentPO: String,
+  permanentUpazila: String,
+  permanentZila: String,
+  guardianName: String,
+  guardianPhone: String,
+  guardianVillage: String,
+  guardianPO: String,
+  guardianRelation: String,
+  previousSchoolName: String,
+  passedYear: String,
+  classWantToTakeAdmission: String,
+  birthCertificateNum: String,
+  yearlyIncomeInFamily: String,
+  membersInFamily: String,
+  rollNumber: String,
 });
 
 const Admission = mongoose.model("Admission", admissionSchema);
